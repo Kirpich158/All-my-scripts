a2ed631ee19ee64f98e1990820cdcad1ceec90ea
Kirill <44980707+Kirpich158@users.noreply.github.com>
2021-07-19 19:59:29 +0300

    Add files via upload

---
+++ DragnDrop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragnDrop : MonoBehaviour
+{
+    public GameObject selectedPiece;
+    int OIL = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+            Debug.Log("Exit");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.transform.CompareTag("Puzzle"))
+            {
+                if (!hit.transform.GetComponent<piecesScript>().inRightPosition)
+                {
+                    selectedPiece = hit.transform.gameObject;
+                    selectedPiece.GetComponent<piecesScript>().selected = true;
+                    selectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(selectedPiece != null)
+            {
+                selectedPiece.GetComponent<piecesScript>().selected = false;
+                selectedPiece = null;
+            }                    
+        }
+
+        if(selectedPiece != null)
+        {
+            Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedPiece.transform.position = new Vector3(mousePoint.x, mousePoint.y, 0);
+        }
+    }
+}
---
+++ EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float minY = -4.4f, maxY = 4.4f;
+    public GameObject asteroidPrefab; //public GameObject[] asteroidPrefabs; for multiple prefabs
+    public GameObject enemyPrefab; //public GameObject[] enemyPrefabs; for multiple prefabs
+    public float timer = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnEnemies", timer);
+    }
+
+    void SpawnEnemies()
+    {
+        float posY = Random.Range(minY, maxY);
+        Vector3 temp = transform.position;
+        temp.y = posY;
+        if(Random.Range(0, 2) > 0)
+        {
+            Instantiate(asteroidPrefab, temp, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(enemyPrefab, temp, Quaternion.Euler(0f, 0f, 90f));
+        }
+
+        Invoke("SpawnEnemies", timer);
+    }
+}
