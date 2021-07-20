9d3a6deeec36fbf56256d2f67caaa8f769a0c7e1
Kirill <44980707+Kirpich158@users.noreply.github.com>
2021-07-20 11:33:17 +0300

    Add files via upload

---
+++ BoxRunner/GameOverScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        ScoreScript.scoreValue = 0;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
