8803665a6381f465a3947ce605c34ef310110adb
Kirill <44980707+Kirpich158@users.noreply.github.com>
2021-07-20 11:47:44 +0300

    Add files via upload

---
+++ SIS-Project/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    Vector2 movement;
+    private Vector3 mousePosition;
+    private Camera mainCamera;
+    public float RotationSpeed;
+
+    //private float moveInput;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        AimRotation();
+    }
+    void FixedUpdate()
+    {//movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if ((movement.x == 0) && (movement.y == 0))
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else { anim.SetBool("isRunning", true); 
+        }
+    }
+
+    void AimRotation()
+    {
+        if (Vector2.Distance(mousePosition, transform.position) > 0.1f)
+        {
+            Vector2 lookDir = mousePosition - transform.position;
+            float angel = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg + 90f;
+            Quaternion rotation = Quaternion.AngleAxis(angel, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
+        }
+    }
+
+}
---
+++ SIS-Project/SceneManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void NextScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+        
+        //if (Input.GetKey(KeyCode.Space)) //for testing
+        //{
+        //    SceneManager.LoadScene(0);
+        //}
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
