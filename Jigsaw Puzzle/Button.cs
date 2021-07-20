d967b4770972a3fdf49eb43c1c09cbed50251f4e
Kirill <44980707+Kirpich158@users.noreply.github.com>
2021-07-20 12:00:34 +0300

    Add files via upload

---
+++ Jigsaw Puzzle/Button.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ChooseLvl()
+    {
+        SceneManager.LoadScene("Choose");
+    }
+
+    public void ChooseLvl1()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ChooseLvl2()
+    {
+        SceneManager.LoadScene("Game2");
+    }
+
+    public void ChooseLvl3()
+    {
+        SceneManager.LoadScene("Game3");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Back2Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
