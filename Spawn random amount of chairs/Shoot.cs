6b5d3888f3ec745c2f790f4c90eaf6ebc592d0ef
Kirill <44980707+Kirpich158@users.noreply.github.com>
2022-01-22 15:47:09 +0300

    Spawn random amount of chairs

---
+++ Spawn random amount of chairs/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabToInstantiate;
+    [SerializeField] private float speed = 7.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Shooting();
+        }
+    }
+
+    private void Shooting() {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        GameObject newGameObject = Instantiate(prefabToInstantiate, mouseRay.origin, Quaternion.identity);
+        Rigidbody rb = newGameObject.GetComponent<Rigidbody>();
+
+        if (rb != null) {
+            rb.velocity = mouseRay.direction * speed;
+        }
+    }
+}
---
+++ Spawn random amount of chairs/Spawning.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Spawning : MonoBehaviour {
+    [SerializeField] private GameObject prefab;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            Spawn();
+        }
+    }
+
+    private void Spawn() {
+        int rndCount = Random.Range(1, 11);
+        transform.position = new Vector3(0f, 0.5f, 0f);
+        for (int i = 0; i < Random.Range(1, 11); i++) {
+            for (int j = 0; j < rndCount; j++) {
+                Instantiate(prefab, transform.position, Quaternion.identity);
+                transform.position = new Vector3(transform.position.x - 1f, transform.position.y, transform.position.z);
+            }
+            transform.position = new Vector3(0f, transform.position.y, transform.position.z + 1.5f);
+        }
+    }
+}
