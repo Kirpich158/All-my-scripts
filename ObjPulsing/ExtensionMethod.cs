07017704ae8cfbf37ecae838cc71b41848b034fc
Kirill <44980707+Kirpich158@users.noreply.github.com>
2022-01-22 15:45:36 +0300

    Object is pulsing in scale

---
+++ ObjPulsing/ExtensionMethod.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods {
+
+    private static Transform tran;
+
+    public static float Pulse(this float time, float min, float max, float frequency) {
+
+        if(time == 0f) {
+            return min;
+        }
+        else {
+            float amp = (max - min) / 2;
+            float fin = Mathf.Sin(Time.time * frequency) * amp;
+            return fin;
+        }
+    }
+}
---
+++ ObjPulsing/TestingScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+    void Update()
+    {
+        float sca = Time.time.Pulse(2f, 5f, 1f);
+        transform.localScale = new Vector3(sca, sca, sca);
+    }
+}
