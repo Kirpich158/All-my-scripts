55fec4e497b7a851cf4bbff029c1615e5b3113ac
Kirill <44980707+Kirpich158@users.noreply.github.com>
2022-01-22 15:40:35 +0300

    Simple movement and camera controllers

---
+++ MovementnCamera/CameraRotate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour {
+
+    [SerializeField] private Transform playerTransform;
+
+    private Vector3 camOffset;
+
+    [Range(0.01f, 1f)]
+    [SerializeField] private float smoothFactor = 0.5f;
+
+    [SerializeField] private bool lookAtPlayer = false;
+    [SerializeField] private bool rotateAroundPlayer = true;
+    [SerializeField] private float rotSpeed = 5f;
+
+    private void Start() {
+        camOffset = transform.position - playerTransform.position;
+    }
+
+    private void LateUpdate() {
+        if (rotateAroundPlayer && Input.GetKey(KeyCode.Mouse2)) {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotSpeed, Vector3.up);
+
+            camOffset = camTurnAngle * camOffset;
+        }
+
+        Vector3 newPos = playerTransform.position + camOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        if (lookAtPlayer || rotateAroundPlayer)
+            transform.LookAt(playerTransform);
+    }
+}
---
+++ MovementnCamera/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float gravity = -19.62f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float rotSpeed = 720f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistanse = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistanse, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(x, 0f, z);
+        moveDir.Normalize();
+
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        if (moveDir != Vector3.zero) {
+            Quaternion toRot = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRot, rotSpeed * Time.deltaTime);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
