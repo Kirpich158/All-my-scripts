7261d585473c3a616311873c7a7ba40f7186b628
Kirill <44980707+Kirpich158@users.noreply.github.com>
2022-01-22 15:38:44 +0300

    Text task from month of working practice

---
+++ FileTestTask/SimpleFileMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+
+public static class SimpleFileMethods {
+    public static bool TryToWriteTextDataToFile(string directoryPath,
+        string fileName = null, string fileExtention = null, Action<StreamWriter> writeAction = null) {
+        if (File.Exists(Path.Combine(directoryPath, fileName + fileExtention)) == false || Directory.Exists(directoryPath) == false || writeAction == null) {
+            return false;
+        }
+        else {
+            try {
+                FileStream stream = new FileStream(Path.Combine(directoryPath, fileName + fileExtention), FileMode.Open);
+                StreamWriter sw = new StreamWriter(stream);
+                writeAction(sw);
+                sw.Close();
+                stream.Close();
+                return true;
+            }
+            catch (Exception e) {
+                Debug.Log(e.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ FileTestTask/TestingScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    private string path = "D:\\FileTextTest\\TextTesting";
+    private string file = "bot";
+    private string extension = ".txt";
+
+    private void Start() {
+        if(SimpleFileMethods.TryToWriteTextDataToFile(path, file, extension, writer => {
+            writer.WriteLine("Testing system...");
+            writer.WriteLine("...Nitro5...");
+            writer.WriteLine("...a lot of rasengans found!");
+        })) {
+            Debug.Log("Ramen");
+        }
+    }
+}
